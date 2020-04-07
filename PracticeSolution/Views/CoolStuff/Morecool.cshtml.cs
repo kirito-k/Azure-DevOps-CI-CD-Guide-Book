c4e93d07479b354a133dcae785f13027377c2ec4
kirito-k <devavratk96@gmail.com>
2020-04-07 12:32:49 -0400

    Adding project files

---
+++ PracticeSolution/Views/CoolStuff/Morecool.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PracticeSolution.Views.CoolStuff
+{
+    public class Morecool : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
